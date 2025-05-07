9a16d6b8677c9292118d6ebff58160be8b1c405d
alekhya rani <alekhyaaarani@gmail.com>
2025-05-07 18:16:46 +0530

    comited

---
+++ task1/script.js
@@ -0,0 +1,9 @@
+document.getElementById('joinBtn').addEventListener('click', function() {
+    alert('Thank you for joining! Check your email for welcome resources.');
+  });
+  
+  document.getElementById('backTop').addEventListener('click', function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+  
+  
